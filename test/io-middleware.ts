fac4d89d2b204cb14c6966d90f88bd5113b802b3
John Wright <jrite@protonmail.com>
2024-01-17 14:02:20 +0000

    test: add typing test

--- test/io-middleware.ts
+++ test/io-middleware.ts
@@ -3,7 +3,7 @@ import request from 'supertest'
 import assert from 'node:assert'
 import { test } from 'node:test'
 import { setTimeout } from 'node:timers/promises'
-import { HALT, ioMiddleware } from '../src/index.js'
+import { HALT, IOMiddleware, ioMiddleware } from '../src/index.js'
 
 test('passes state from one middleware to the next', (_, done) => {
   const app = express().use(
@@ -49,4 +49,23 @@ test('stopping middleware from continuing', (_, done) => {
   )
 
   request(app).get('/').expect(200).end(done)
+})
+
+test('types with separate functions', () => {
+  function foo(): IOMiddleware<{}, { foo: string }> {
+    return (_req, _res, state) => ({ ...state, foo: 'bar' })
+  }
+
+  function success(): IOMiddleware<
+    { foo: string },
+    { foo: string; mung: number }
+  > {
+    return (_req, _res, state) =>
+      state.foo === 'rab' ? HALT : { ...state, mung: 1 }
+  }
+
+  express().use(ioMiddleware({}, foo(), success()))
+
+  // @ts-expect-error Argument of type '{}' is not assignable to parameter of type '{ foo: string; }'.
+  express().use(ioMiddleware({}, success()))
 })
